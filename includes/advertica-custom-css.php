ec17e62181c16cd51e9a8d08eaf3aa15ab0b4526
rvwest <richard.west@jisc.ac.uk>
2016-08-21 22:57:12 +0100

    Article - Improving blockquotes

--- includes/advertica-custom-css.php
+++ includes/advertica-custom-css.php
@@ -1,19 +1,19 @@
 <?php
- 
+
 	$bg_color = esc_attr( get_theme_mod('advertica_lite_pri_color', '#FFA500') );
 	$fullparallax_image = esc_url( get_theme_mod('parallax_image', get_template_directory_uri().'/images/Parallax_Section_Image.jpg') );
 	$breadbgcolor = esc_attr( get_theme_mod('breadcrumbbg_color', '#F2F2F2') );
 	$breadimage = esc_url( get_theme_mod('breadcrumbbg_image', get_template_directory_uri().'/images/page-title-bg.jpg') );
-	
+
 ?>
 <style type="text/css">
 
-	/***************** THEME *****************/	
+	/***************** THEME *****************/
   	 a.skt-featured-icons,.service-icon{ background: <?php if(isset($bg_color)){ echo $bg_color; } ?>;}
 	 a.skt-featured-icons:after,.service-icon:after {border-top-color: <?php if(isset($bg_color)){ echo $bg_color; } ?>; }
 	 a.skt-featured-icons:before,.service-icon:before {border-bottom-color: <?php if(isset($bg_color)){ echo $bg_color; } ?>; }
 
-	
+
 	.continue a,.sticky-post {
 		color : <?php if(isset($bg_color)){ echo $bg_color; } ?>;
 		border-color:<?php if(isset($bg_color)){ echo $bg_color; } ?>
@@ -25,7 +25,7 @@
 		background: <?php if(isset($bg_color)){ echo $bg_color; } ?>;
 	}
 	.social li a:hover:before{color:#fff; }
-	a#backtop,#respond input[type="submit"]:hover,.skt-ctabox div.skt-ctabox-button a:hover,.widget_tag_cloud a:hover,.continue a:hover,blockquote,.skt-quote,#advertica-paginate .advertica-current,#advertica-paginate a:hover,.postformat-gallerydirection-nav li a:hover,#wp-calendar,.comments-template .reply a:hover,#content .contact-left form input[type="submit"]:hover,.skt-parallax-button:hover,.sktmenu-toggle {
+	a#backtop,#respond input[type="submit"]:hover,.skt-ctabox div.skt-ctabox-button a:hover,.widget_tag_cloud a:hover,.continue a:hover,.skt-quote,#advertica-paginate .advertica-current,#advertica-paginate a:hover,.postformat-gallerydirection-nav li a:hover,#wp-calendar,.comments-template .reply a:hover,#content .contact-left form input[type="submit"]:hover,.skt-parallax-button:hover,.sktmenu-toggle {
 		background-color: <?php if(isset($bg_color)){ echo $bg_color; } ?>;
 	}
 	.skt-ctabox div.skt-ctabox-button a,#portfolio-division-box .readmore,.teammember,.comments-template .reply a,#respond input[type="submit"],.slider-link a,.ske_tab_v ul.ske_tabs li.active,.ske_tab_h ul.ske_tabs li.active,#content .contact-left form input[type="submit"],.filter a,.skt-parallax-button,#advertica-paginate a:hover,#advertica-paginate .advertica-current,#content .contact-left form textarea:focus,#content .contact-left form input[type="text"]:focus, #content .contact-left form input[type="email"]:focus, #content .contact-left form input[type="url"]:focus, #content .contact-left form input[type="tel"]:focus, #content .contact-left form input[type="number"]:focus, #content .contact-left form input[type="range"]:focus, #content .contact-left form input[type="date"]:focus, #content .contact-left form input[type="file"]:focus {
@@ -72,20 +72,20 @@
 
 	.full-bg-breadimage-fixed {}
 	#full-division-box { background-image: url("<?php if(isset($fullparallax_image)){ echo $fullparallax_image; } else { echo get_template_directory_uri().'/images/Parallax_Section_Image.jpg'; } ?>"); }
-	
+
 	<?php if ( $breadimage != '' ) { ?>
 		.full-bg-breadimage-fixed { background-image: url("<?php echo $breadimage; ?>"); }
 	<?php } else { ?>
 		.full-bg-breadimage-fixed { background-color: <?php echo $breadbgcolor; ?>; }
 	<?php } ?>
-	
+
 	.bread-title-holder .skt-breadcrumbs-separator,.bread-title-holder h1.title,.cont_nav_inner span,.bread-title-holder .cont_nav_inner p,.bread-title-holder .cont_nav_inner a{
 		color: <?php if(isset($skt_bread_title_color)){ echo $skt_bread_title_color; } ?>;
 	}
-	
+
 	/***************** PAGINATE *****************/
-	
-	
+
+
 	#skenav .ske-menu #menu-secondary-menu li a:hover, #skenav .ske-menu #menu-secondary-menu .current-menu-item a{color: #71C1F2;  }
 	.footer-seperator{background-color: rgba(0,0,0,.2);}
 	#skenav .ske-menu #menu-secondary-menu li .sub-menu li {	margin: 0;  }
