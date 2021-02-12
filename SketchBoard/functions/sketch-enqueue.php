a7d42e496dd31a78fd14470c102710e6d736ff29
rvwest <richard.west@jisc.ac.uk>
2021-02-12 14:54:48 +0000

    1col - 400 font weight

--- SketchBoard/functions/sketch-enqueue.php
+++ SketchBoard/functions/sketch-enqueue.php
@@ -44,7 +44,7 @@ wp_enqueue_script('advertica-lite-waypoints-js',get_template_directory_uri().'/j
 	/*wp_enqueue_style( 'advertica-lite-bootstrap-stylesheet', get_template_directory_uri().'/css/bootstrap-responsive.css', false, $theme->Version);*/
 
 	// Google Fonts
-	wp_enqueue_style('advertica-lite-googlefont-opensans', '//fonts.googleapis.com/css?family=Open+Sans:700,400,300&subset=latin,latin-ext');
+	wp_enqueue_style('advertica-lite-googlefont-opensans', '//fonts.googleapis.com/css?family=Open+Sans:700,600,400,300&subset=latin,latin-ext');
 /*	wp_enqueue_style('advertica-lite-googlefont-raleway', '//fonts.googleapis.com/css?family=Raleway:900,700,400');*/
 }
 add_action('wp_enqueue_scripts', 'advertica_lite_theme_stylesheet');
