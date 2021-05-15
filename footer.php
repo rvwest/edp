9f51e80b5ddb1851ce3c050f5fb8c077d52b782a
rvwest <richard.west@jisc.ac.uk>
2021-05-15 15:59:35 +0100

    Ecologi logo added to footer

--- footer.php
+++ footer.php
@@ -27,11 +27,16 @@
 			<div class="row-fluid">
 
 
-
+<div class="span8">
 <p class="foot-message"><a href="https://twitter.com/edpsyuk"><i class="fab fa-twitter"></i> @edpsyuk</a> | <a href="mailto:hello@edpsy.org.uk"><i class="fal fa-envelope"></i> hello@edpsy.org.uk</a></p>
 <p class="foot-message"><a href="/about/">Find out more about us</a><br/>Spotted something wrong? <a href="mailto:hello@edpsy.org.uk">let us know</a></p>
 <p class="foot-message"><a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Reusing our content</a> | <a href="/cookies">Cookies and privacy</a></p>
-
+</div>
+<div class="span4 foot-message footer-accreditation">
+<a href="https://ecologi.com/edpsyltd" target="_blank" rel="noopener noreferrer" title="View our Ecologi profile" style="width:200px;display:inline-block;">
+  <img alt="We offset our carbon footprint via Ecologi" src="https://api.ecologi.com/badges/cpw/60292431878cf1001cb5ce58?white=true&landscape=true" style="width:200px;" />
+</a>
+</div>
 				<div class="clearfix"></div>
 			</div>
 		</div>
