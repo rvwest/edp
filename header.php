2a8ea204d4838e51f372c2c79e839f4a80d47d8f
rvwest <richard.west@jisc.ac.uk>
2022-01-31 09:57:14 +0000

    Access event

--- header.php
+++ header.php
@@ -26,13 +26,7 @@
 </head>
 <body <?php body_class(); ?>  >
 
-<div class="cta-header-block">
-	<div class="container">
-		<div class="row-fluid fixedrow">
-			<p><i class="far fa-briefcase"></i>&nbsp; <s>£450</s> <strong>£300</strong> to <a href="https://edpsy.org.uk/jobs/post/">post a job with us</a> and reach thousands of people</p><p class="cta-second-link"><a href="https://edpsy.org.uk/jobs/choosing-edpsy-jobs/" >Find out more</a></p> 
-		</div>
-	</div>
-</div>
+<div class="cta-header-block cta-header-block--green"><div class="container"><div class="row-fluid fixedrow"><p><i class="far fa-calendar-alt"></i>&nbsp; Another chance to catch our popular webinar: <a href="https://edpsy.org.uk/event/webinar-an-introduction-to-creating-accessible-content-as-psychologists/">Creating accessible content as psychologists</a></p><p class="cta-second-link"><a href="https://edpsy.org.uk/event/webinar-an-introduction-to-creating-accessible-content-as-psychologists/" >Find out more</a></p></div></div> </div>
 		<div id="wrapper" class="skepage " >
 		<div id="header" class="skehead-headernav clearfix">
 			<div class="glow">
