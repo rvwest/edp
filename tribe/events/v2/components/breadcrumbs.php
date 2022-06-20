25543398512120effd3c31f04de1e49916641bca
rvwest <github@rvwest.co.uk>
2022-06-20 08:09:50 +0100

    bredcrumb fixing

---
+++ tribe/events/v2/components/breadcrumbs.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * View: Breadcrumbs
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/v2/components/breadcrumbs.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 4.9.11
+ *
+ * @var array $breadcrumbs An array of data for breadcrumbs.
+ */
+?>
+
+<div class="bread-title-holder">
+		<div class="container">
+			<div class="row-fluid">
+				<section class="cont_nav"><div class="cont_nav_inner"><p><a href="https://localhost/">Home</a>&nbsp;<span class="skt-breadcrumbs-separator"> &gt;&gt; </span>&nbsp;<span>Events</span></p></div></section>	
+				<div class="container_inner clearfix">
+		
+					
+				</div>		</div>
+		</div>
+	</div> 	
