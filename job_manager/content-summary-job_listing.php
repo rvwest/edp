ef0d45ed250c459e07ccc8696e537eecf7bae137
rvwest <github@rvwest.co.uk>
2022-07-29 16:21:03 +0100

    YMAL - jobs styling

--- job_manager/content-summary-job_listing.php
+++ job_manager/content-summary-job_listing.php
@@ -21,12 +21,11 @@ global $job_manager;
 	<?php if ( $logo = get_the_company_logo() ) : ?>
 		<figure>
 			<img src="<?php echo esc_url( $logo ); ?>" alt="<?php the_company_name(); ?>" title="<?php the_company_name(); ?>" class="crp_thumb crp_featured crp_job_logo" />
-			<img width="480" height="280" src="http://localhost/wp-content/uploads/2022/06/john-schaidler-9V3Q2W_mRLE-unsplash-480x280.jpg" class="attachment-edpsy_loop_img size-edpsy_loop_img wp-post-image" alt="" loading="lazy">
 	</figure>
 	<?php endif; ?>
-	<span class="crp_title"><span class="title-tag"><i class="far fa-bell-on"></i> New job: </span><?php wpjm_the_job_title(); ?>
+	<div class="crp_title"><span class="title-tag"><i class="far fa-bell-on"></i> New job: </span><?php wpjm_the_job_title(); ?>
 		- <?php the_job_location( false ); ?>
-	</span>
+	</div>
 	</div>
 </a>
 
