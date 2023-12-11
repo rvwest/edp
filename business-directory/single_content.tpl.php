489803828c75fbb38dbb005822d665c9db428949
rvwest <github@rvwest.co.uk>
2023-12-11 13:32:15 +0000

    Thesis directory 3

--- business-directory/single_content.tpl.php
+++ business-directory/single_content.tpl.php
@@ -22,35 +22,39 @@
 <?php echo $fields->data_analysis->html; ?>
 </div>
 <?php if ( $fields->full_text_thesis->html > "" | $fields->published_paper->html > "" | $fields->blog__news_story->html > "" | $fields->website->html > "" ) : ?>
-    <h3>Further reading</h3>
+    <div class="wpbdp-further-reading-block">
+    <h2>Further reading</h2>
     <ul>
 <?php endif; ?>
 
 <?php if ( $fields->full_text_thesis->html > "" ) : ?>
     <li>
-        <a href="<?php echo $fields->full_text_thesis->raw; ?>">Full text thesis</a>
+        <a href="<?php echo $fields->full_text_thesis->raw[0]; ?>">Full text thesis</a>
 </li>
 <?php endif; ?>
 
 
 <?php if ( $fields->published_paper->html > "" ) : ?>
     <li>
-        <a href="<?php echo $fields->published_paper->raw; ?>">Published paper</a>
+        <a href="<?php echo $fields->published_paper->raw[0]; ?>">Published paper</a>
 </li>
 <?php endif; ?>
 
 <?php if ( $fields->blog__news_story->html > "" ) : ?>
     <li>
-        <a href="<?php echo $fields->blog__news_story->raw; ?>">Blog / news story</a>
+        <a href="<?php echo $fields->blog__news_story->raw[0]; ?>">Blog / news story</a>
 </li>
 <?php endif; ?>
-<li>
+
 <?php if ( $fields->website->html > "" ) : ?>
+    <li>
     <?php echo $fields->website->html; ?>
 </li>
 <?php endif; ?>
 </ul>
+</div>
 
 <h2>Abstract</h2>
-<?php echo $fields->abstract->raw; ?>
+<?php echo $fields->abstract->html; ?>
+<h2 class="wpbdp-contact">Contact <?php echo $fields->name->raw; ?></h2>
 <?php echo $fields->email->html; ?>
