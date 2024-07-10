b5f07dc69e1b294cf47efb26a5d19d31908995b2
rvwest <github@rvwest.co.uk>
2024-07-10 11:47:05 +0200

    Banner removed, bell fixed,
     sass settings tweaked

--- header.php
+++ header.php
@@ -31,14 +31,7 @@
 <body <?php body_class(); ?>>
 
 	<!--promospace -->
-	<div class="cta-header-block cta-header-block--refugee">
-		<div class="container">
-			<div class="row-fluid fixedrow">
-				<p><img height="20px" src="<?php echo get_template_directory_uri(); ?>/images/reuk_logo_only.png" />&nbsp;<a href="https://www.justgiving.com/page/edpsy-orguk-special-edition-reuk">Support refugee children</a> to access and thrive in education</p>
-				<p class="cta-second-link"><a href="https://www.justgiving.com/page/edpsy-orguk-special-edition-reuk">Donate to Refugee Education UK</a></p>
-			</div>
-		</div>
-	</div>
+	
 <!--/promospace-->
 	<div id="wrapper" class="skepage ">
 		<div id="header" class="skehead-headernav clearfix">
