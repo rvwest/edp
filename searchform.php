2e48c3209969fa5d33b60eba9ad5a5fae80ad31a
rvwest <github@rvwest.co.uk>
2024-08-31 16:48:09 +0200

    translation templates language

--- searchform.php
+++ searchform.php
@@ -2,6 +2,7 @@
 	<div class="searchleft">
 		<input type="text" value="" placeholder="<?php _e('Search','advertica-lite');?>" name="s" id="searchbox" class="searchinput"/>
 	</div>
+	<input type="hidden" name="post_type" value="any" />
     <div class="searchright">
     	<input type="submit" class="submitbutton" value="<?php _e('Search','advertica-lite');?>" />
     </div>
